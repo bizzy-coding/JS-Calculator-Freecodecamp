4864d5f155a18d1f28a3196703d4ae93013c1cc2
Bizzy-coding <bizzycoding@gmail.com>
2023-04-03 20:01:55 +0100

    adding event listener to keys

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.calculator__keys');
+
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+        console.log('itsa button')
+    }
+})
