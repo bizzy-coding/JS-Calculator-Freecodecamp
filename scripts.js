d27c9199e724422b8a9df0977ed9fd31affca086
Bizzy-coding <bizzycoding@gmail.com>
2023-04-05 19:37:18 +0100

    continuing calculations via different operator clicks

--- scripts.js
+++ scripts.js
@@ -10,19 +10,20 @@ keys.addEventListener("click", (e) => {
     const displayedNum = display.textContent;
     const previousKeyType = calculator.dataset.previousKeyType;
 
-    Array.from(key.parentNode.children)
-      .forEach(k => k.classList.remove('is-depressed'));
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
 
     if (!action) {
       console.log("number key!");
-      if (displayedNum === '0' || previousKeyType === 'operator') {
+      if (displayedNum === "0" || previousKeyType === "operator") {
         display.textContent = keyContent;
       } else {
         console.log("bigger than 1");
         display.textContent = displayedNum + keyContent;
       }
+      calculator.dataset.previousKeyType = "number";
     }
-
 
     if (
       action === "add" ||
@@ -30,49 +31,58 @@ keys.addEventListener("click", (e) => {
       action === "multiply" ||
       action === "divide"
     ) {
-      console.log("operator key!");
-      key.classList.add('is-depressed');
-      calculator.dataset.previousKeyType = 'operator';
-      calculator.dataset.firstValue = displayedNum
-      calculator.dataset.operator = action
-    }
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
 
-    if (action === 'decimal') {
-        if (!displayedNum.includes('.')) {
-          display.textContent = displayedNum + '.'
-          //for decimal point - no duplication
-        } else if (previousKeyType === 'operator') {
-          display.textContent = '0.'
-        }
-        calculator.dataset.previousKeyType = 'decimal'
+      // Note: It's sufficient to check for firstValue and operator because secondValue always exists
+      if (firstValue && operator && previousKeyType !== "operator") {
+        display.textContent = calculate(firstValue, operator, secondValue);
       }
 
+      key.classList.add("is-depressed");
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+    //for decimal point - no duplication & display zero after .
+    if (action === "decimal") {
+      if (!displayedNum.includes(".")) {
+        display.textContent = displayedNum + ".";
+      } else if (previousKeyType === "operator") {
+        display.textContent = "0.";
+      }
+      calculator.dataset.previousKeyType = "decimal";
+    }
+
     if (action === "clear") {
-      console.log("clear key!");
+      calculator.dataset.previousKeyType = "clear";
+      console.log(calculator.dataset.previousKeyType);
     }
 
     if (action === "calculate") {
-        const firstValue = calculator.dataset.firstValue;
-        const operator = calculator.dataset.operator;
-        const secondValue = displayedNum;
-        display.textContent = calculate(firstValue, operator, secondValue)
-        console.log("equal key!");
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+      display.textContent = calculate(firstValue, operator, secondValue);
+      console.log("equal key!");
+      calculator.dataset.previousKeyType = "calculate";
     }
   }
 });
 
 const calculate = (n1, operator, n2) => {
-    let result = ''
-  
-    if (operator === 'add') {
-      result = parseFloat(n1) + parseFloat(n2)
-    } else if (operator === 'subtract') {
-      result = parseFloat(n1) - parseFloat(n2)
-    } else if (operator === 'multiply') {
-      result = parseFloat(n1) * parseFloat(n2)
-    } else if (operator === 'divide') {
-      result = parseFloat(n1) / parseFloat(n2)
-    }
-  
-    return result
+  let result = "";
+
+  if (operator === "add") {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === "subtract") {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === "multiply") {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === "divide") {
+    result = parseFloat(n1) / parseFloat(n2);
   }
+
+  return result;
+};
